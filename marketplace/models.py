dad453b21e0667d0c4ffff2a1460659a42d752d0
Alex Williams <ahnnwilliams@hotmail.co.uk>
2023-11-28 21:31:39 +0000

    completed Store model w migrations

--- marketplace/models.py
+++ marketplace/models.py
@@ -7,10 +7,17 @@ from django.contrib.auth.models import User
 class Store(models.Model):
     store_name = models.CharField(max_length=255, blank=False, null=False)
     store_email = models.EmailField(blank=False, null=False)
-    phone_number = models.IntegerField(blank=False)
+    phone_number = models.CharField(blank=False, max_length=13)
     description = models.TextField(validators=[
             MinLengthValidator(100, 'Please enter a description of at least 100 characters')
         ])
     user_id = models.ForeignKey(User, related_name='store_owner', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.store_name
 
+    class Meta:
+        ordering=['store_name']
+
+class Profile(models.Model):
+    
