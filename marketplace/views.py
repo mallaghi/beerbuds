70009c10ff026d6ccd7e617cc321bc75edad43d2
Maram Allaghi <98703435+mallaghi@users.noreply.github.com>
2023-12-04 23:27:14 +0300

    Revert "Views beerbuds"

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Store, Beer, Profile
 
-
-def beer_index(request):
-  beers = Beer.objects.all()
-  return render(request,'marketplace/beer_index.html', {'beers': beers })
+# Create your views here.
