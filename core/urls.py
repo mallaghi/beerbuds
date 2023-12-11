6d32335749b55c3299f2f15ea919db2071906b15
Alex Williams <ahnnwilliams@hotmail.co.uk>
2023-12-11 19:20:48 +0000

    created home page template and added html

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ app_name = 'core'
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html',
-        authentication_form=LoginForm), name='login')
+        authentication_form=LoginForm), name='login'),
+    path('', views.home, name='home')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'core/home.html')
 
 def signup(request):
     if request.method == 'POST':
@@ -12,4 +16,3 @@ def signup(request):
         form = SignupForm()
 
     return render(request, 'core/signup.html', {'form': form})
-
