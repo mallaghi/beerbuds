01f32e83f65c1cf7b4f432a898fd0a63b9a3f6d0
Brett Posner <brett.posner@gmail.com>
2024-02-27 15:25:11 +0000

    general styling sorted for show page and favourite button integrated as icon

--- user_actions/forms.py
+++ user_actions/forms.py
@@ -8,12 +8,12 @@ class ReviewForm(forms.ModelForm):
         exclude = ('profile_id', 'beer_id')
 
     review_content = forms.CharField(widget=forms.Textarea(attrs={
-        'placeholder': 'Your review (minimum 50 characters)',
+        'placeholder': 'Your review (minimum 10 characters)',
         'class': 'w-full py-4 px-6 rounded-xl border-2 border-gray-200'
     }))
 
     rating = forms.IntegerField(widget=forms.NumberInput(attrs={
-        'placeholder': 'Your rating',
+        'placeholder': 'Your rating 1-10',
         'class': 'w-full py-4 px-6 rounded-xl border-2 border-gray-200'
     }))
 
